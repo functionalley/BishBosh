752a32cfa8151faaec4a5c39b7ce5cc4a147e323
Alistair Ward <git@functionalley.com>
2021-11-16 13:20:36 +0000

    Removed unused unboxed array type.

--- src-lib/BishBosh/Attribute/Rank.hs
+++ src-lib/BishBosh/Attribute/Rank.hs
@@ -31,7 +31,6 @@ module BishBosh.Attribute.Rank(
 -- ** Type-synonyms
 	EvaluateRank,
 	ArrayByRank,
-	UArrayByRank,
 -- ** Data-types
 	Rank(..),
 -- * Constants
@@ -64,7 +63,6 @@ import qualified	BishBosh.Type.Count			as Type.Count
 import qualified	Control.DeepSeq
 import qualified	Control.Exception
 import qualified	Data.Array.IArray
-import qualified	Data.Array.Unboxed
 import qualified	Data.Char
 import qualified	Data.List
 import qualified	Data.Ord
@@ -208,9 +206,6 @@ initialAllocationByRankPerSide	= listArrayByRank [8, 2, 2, 2, 1, 1]
 
 -- | A boxed array indexed by /rank/, of arbitrary values.
 type ArrayByRank	= Data.Array.IArray.Array Rank
-
--- | An unboxed array indexed by /rank/, of fixed-size values.
-type UArrayByRank	= Data.Array.Unboxed.UArray Rank
 
 -- | Array-constructor from an ordered list of elements.
 listArrayByRank :: Data.Array.IArray.IArray a e => [e] -> a Rank e
